40738b4b7fbfeedadf19a58ff04b5145cfe4e3dc
duyvu1893 <duyvu1893@users.noreply.github.com>
2016-05-03 11:54:02 -0500

    Pep8 Compliance

--- Movie.py
+++ Movie.py
@@ -1,8 +1,14 @@
 import webbrowser
-class Movie():
-    def __init__(self,title,image_link,trailer_link):
-        self.title=title
-        self.poster_image_url=image_link
-        self.trailer_youtube_url=trailer_link
+
+
+# Database of movies
+class Movie:
+    """ Contains information about movies """
+    def __init__(self, title, image_link, trailer_link):
+        self.title = title
+        self.poster_image_url = image_link
+        self.trailer_youtube_url = trailer_link
+
+    # Open web browser to show trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_link)
